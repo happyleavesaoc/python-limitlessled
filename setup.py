7bb343bec7a7dadf796bb3e2f25333daa784cec0
Sören Oldag <soeren_oldag@freenet.de>
2017-01-02 01:51:38 +0100

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
-    packages=['limitlessled', 'limitlessled.group'],
+    packages=['limitlessled', 'limitlessled.group', 'limitlessled.group.commands'],
     install_requires=[],
     classifiers=[
         'License :: OSI Approved :: MIT License',
