012155da041ed8a3cbf01a7b3e0da8fc6a25ea24
happyleaves <happyleaves.tfr@gmail.com>
2017-05-16 18:21:38 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='limitlessled',
-    version='1.0.7',
+    version='1.0.8',
     description='Control LimitlessLED products.',
     url='https://github.com/happyleavesaoc/python-limitlessled/',
     license='MIT',
