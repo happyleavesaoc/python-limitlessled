7789063c9188e3c521ae5221ca6017814d87e630
rubenverhoef <ruben_verhoef@hotmail.com>
2018-02-15 20:43:52 +0100

    Code cleanup after testing. Unused code deleted.

--- limitlessled/group/dimmer.py
+++ limitlessled/group/dimmer.py
@@ -23,7 +23,6 @@ class DimmerGroup(Group):
         :param name: Group name.
         """
         super().__init__(bridge, number, name, DIMMER)
-        # self._temperature = 0.5
 
     @property
     def brightness(self):
@@ -48,98 +47,48 @@ class DimmerGroup(Group):
                          self._dimmest, self._brightest,
                          self._to_brightness)
 
-    # @property
-    # def temperature(self):
-    #     """ Temperature property.
-    #
-    #     :returns: Temperature (0.0-1.0)
-    #     """
-    #     return self._temperature
-    #
-    # @temperature.setter
-    # def temperature(self, temperature):
-    #     """ Set the temperature.
-    #
-    #     :param temperature: Value to set (0.0-1.0).
-    #     """
-    #     try:
-    #         cmd = self.command_set.temperature(temperature)
-    #         self.send(cmd)
-    #         self._temperature = temperature
-    #     except AttributeError:
-    #         self._setter('_temperature', temperature,
-    #                      self._coolest, self._warmest,
-    #                      self._to_temperature)
-
     def transition(self, duration, brightness=None):
-    # def transition(self, duration, brightness=None, temperature=None):
         """ Transition wrapper.
 
         Short-circuit transition if necessary.
 
         :param duration: Duration of transition.
         :param brightness: Transition to this brightness.
-        :param temperature: Transition to this temperature.
         """
-        # Transition immediately if duration is zero.
         if duration == 0:
             if brightness is not None:
                 self.brightness = brightness
-            # if temperature is not None:
-            #     self.temperature = temperature
             return
         if brightness != self.brightness:
             self._transition(duration, brightness)
-        # if brightness != self.brightness or temperature != self.temperature:
-        #     self._transition(duration, brightness, temperature)
 
     @rate(reps=1)
     def _transition(self, duration, brightness):
-    # def _transition(self, duration, brightness, temperature):
         """ Complete a transition.
 
         :param duration: Duration of transition.
         :param brightness: Transition to this brightness.
-        :param temperature: Transition to this temperature.
         """
         # Set initial value.
         b_start = self.brightness
-        # t_start = self.temperature
         # Compute ideal step amount.
         b_steps = 0
         if brightness is not None:
             b_steps = steps(self.brightness, brightness,
                             self.command_set.brightness_steps)
-        # t_steps = 0
-        # if temperature is not None:
-        #     t_steps = steps(self.temperature, temperature,
-        #                     self.command_set.temperature_steps)
         # Compute ideal step amount (at least one).
-        total_steps = b_steps
-        total_commands = b_steps
-        # total_steps = max(b_steps, t_steps, 1)
-        # total_commands = b_steps + t_steps
         # Calculate wait.
-        wait = self._wait(duration, total_steps, total_commands)
+        wait = self._wait(duration, b_steps, b_steps)
         # Scale down steps if no wait time.
         if wait == 0:
-            b_steps = self._scale_steps(duration, total_commands,
+            b_steps = self._scale_steps(duration, b_steps,
                                                  b_steps)
-            # b_steps, t_steps = self._scale_steps(duration, total_commands,
-            #                                      b_steps, t_steps)
-            total_steps = b_steps
-            # total_steps = max(b_steps, t_steps, 1)
         # Perform transition.
-        for i in range(total_steps):
+        for i in range(b_steps):
             # Brightness.
-            if b_steps > 0 and i % (total_steps / b_steps) == 0:
-                self.brightness = util.transition(i, total_steps,
+            if b_steps > 0:
+                self.brightness = util.transition(i, b_steps,
                                                   b_start, brightness)
-            # Temperature.
-            # elif t_steps > 0:
-            #     self.temperature = util.transition(i, total_steps,
-            #                                        t_start, temperature)
-            # Wait.
             time.sleep(wait)
 
     def _setter(self, attr, value, bottom, top, to_step):
@@ -169,15 +118,6 @@ class DimmerGroup(Group):
         self._to_value(self._brightness, brightness,
                        self.command_set.brightness_steps,
                        self._dimmer, self._brighter)
-
-    # def _to_temperature(self, temperature):
-    #     """ Step to a given temperature.
-    #
-    #     :param temperature: Get to this temperature.
-    #     """
-    #     self._to_value(self._temperature, temperature,
-    #                    self.command_set.temperature_steps,
-    #                    self._cooler, self._warmer)
 
     @rate(reps=1)
     def _to_value(self, current, target, max_steps, step_down, step_up):
@@ -209,20 +149,6 @@ class DimmerGroup(Group):
                              self.command_set.brightness_steps)):
             self._dimmer()
 
-    # @rate(wait=0.025, reps=2)
-    # def _warmest(self):
-    #     """ Group temperature as warm as possible. """
-    #     for _ in range(steps(self.temperature, 1.0,
-    #                          self.command_set.temperature_steps)):
-    #         self._warmer()
-
-    # @rate(wait=0.025, reps=2)
-    # def _coolest(self):
-    #     """ Group temperature as cool as possible. """
-    #     for _ in range(steps(self.temperature, 0.0,
-    #                          self.command_set.temperature_steps)):
-    #         self._cooler()
-
     def _brighter(self):
         """ One step brighter. """
         self.send(self.command_set.brighter())
@@ -230,11 +156,3 @@ class DimmerGroup(Group):
     def _dimmer(self):
         """ One step dimmer. """
         self.send(self.command_set.dimmer())
-
-    # def _warmer(self):
-    #     """ One step warmer. """
-    #     self.send(self.command_set.warmer())
-
-    # def _cooler(self):
-    #     """ One step cooler. """
-    #     self.send(self.command_set.cooler())
